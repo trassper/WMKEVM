453c00ed74ede0330a96f76d26fc7e0c5cbaa2df
trassper <johannesjakobi98@gmail.com>
2026-03-24 22:57:23 +0100

    Enhance service worker cache management
    Update service worker to delete old caches on activation.

--- sw.js
+++ sw.js
@@ -13,7 +13,18 @@ self.addEventListener('install', event => {
 });
 
 self.addEventListener('activate', event => {
-  event.waitUntil(self.clients.claim());
+  event.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(
+        keys.map(key => {
+          if (key !== CACHE_NAME) {
+            return caches.delete(key);
+          }
+        })
+      )
+    )
+  );
+  self.clients.claim();
 });
 
 self.addEventListener('fetch', event => {
